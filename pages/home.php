e7a1e6fcb7a4f0b0d012acf55723f2b3aab19100
Yago Jesus do Prado <yagojprado@gmail.com>
2025-03-27 22:58:12 -0300

    Add files via upload

---
+++ pages/home.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../assets/js/scripts.js"></script>
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <title>ASTHAMED</title>
+    <link rel="icon" type="image/x-icon" href="../public/images/asthamed-icon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="header-content">
+                <div class="header-left-menu">
+                    <div class="header-contatos">
+                        <div class="header-telefone">
+                            <div class="header-telefone-container">
+                                <i class="bi bi-telephone"></i>
+                            </div>
+                            <div class="header-telefone-infos">
+                                <h5>Telefone</h5>
+                                <p>(62) 9 9999-9999</p>
+                            </div>
+                        </div>
+                        <div class="header-whatsapp">
+                            <div class="header-whatsapp-container">
+                                <i class="bi bi-whatsapp"></i>
+                            </div>
+
+                            <div class="header-whatsapp-infos">
+                                <h5>Whatsapp</h5>
+                                <p>(62) 9 9999-9999</p>
+                            </div>
+                        </div>
+                    </div>
+                    <span class="separador"></span>
+                    <div class="header-nav-bar">
+                        <div class="nav-item"><a href="">Sobre</a></div>
+                        <div class="nav-item"><a href="">Produtos</a></div>
+                        <div class="nav-item"><a href="">Fornecedores</a></div>
+                        <div class="nav-item"><a href="">Depoimentos</a></div>
+                    </div>
+                </div>
+                <div class="header-logo">
+                    <img src="../public/images/asthamed-icon.png" alt="Logo" class="logo-image">
+                </div>
+                <div class="header-right-menu">
+                    <div class="header-buttons">
+                        <div class="button-carrinho">
+                            <i class="bi bi-cart2"></i>
+                            <p>+ CARRINHO</p>
+                        </div>
+                        <div class="button-orcamento-rapido">
+                            <i class="bi bi-currency-dollar"></i>
+                            ORÇAMENTO RÁPIDO
+                        </div>
+                    </div>
+                    <span class="separador"></span>
+                    <div class="header-nav-bar">
+                        <div class="nav-item"><a href="">Dúvidas</a></div>
+                        <div class="nav-item"><a href="">Blog</a></div>
+                        <div class="nav-item"><a href="">Fale Conosco</a></div>
+                        <div class="nav-item"><a href="">Depoimentos</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
--- teste.php
+++ teste.php
@@ -1,1 +1,12 @@
-easfkjhasfjklashfjkahflkashlfkahlkjfhlajksfhsaljkfa
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <script src="/assets/js/scripts.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
